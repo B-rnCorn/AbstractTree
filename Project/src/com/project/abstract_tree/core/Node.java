0023ee68b7759566866fb4202fb206c02c6c83a0
SeregeySulimenko <sergey-sulimenko@mail.ru>
2020-12-07 20:35:21 +0400

    commit

--- Project/src/com/project/abstract_tree/core/Node.java
+++ Project/src/com/project/abstract_tree/core/Node.java
@@ -5,31 +5,30 @@ import java.util.LinkedList;
 public class Node <T> {
     private int idNode;
     private T value;
-    private LinkedList<Node> leftChildrens;
-    private LinkedList<Node> rightChildrens;
+    private Node parent;
+    private LinkedList<Node> Childrens;
+    public Node(int idNode,T value,Node parent){
+        this.idNode=idNode;
+        this.value=value;
+        this.parent=parent;
+    }
     public Node(int idNode,T value){
         this.idNode=idNode;
         this.value=value;
+        this.parent=null;
     }
-    public void setChildren(int idNode,T value){
-        if(idNode>=this.idNode)leftChildrens.add(new Node(idNode,value));
-        else rightChildrens.add(new Node(idNode,value));
-    }
-    public void setChildren(Node node){
-        if(node.getIdNode()>=this.idNode)leftChildrens.add(node);
-        else rightChildrens.add(node);
-    }
-    public int getIdNode(){
-        return idNode;
-    }
+    public void setChildren(int idNode,T value,Node parent){ Childrens.add(new Node(idNode,value,parent)); }
+    public void setChildren(Node node){ Childrens.add(node); }
+    public int getIdNode(){ return idNode; }
     public void setIdNode(int idNode){
         this.idNode=idNode;
     }
     public T getValue() {
         return value;
     }
-
     public void setValue(T value) {
         this.value = value;
     }
+    public Node getParent() { return parent; }
+    public void setParent(Node parent) { this.parent = parent; }
 }
