aef91ba71e3cfd0545abdf7d0e9fc4a7e9fd4deb
Dryuss <gipto93@gmail.com>
2020-12-11 15:35:19 +0400

    Второй коструктор: заменил инициализацию вызовом первого коструктора Добавил пару коментариев Разделил методы пустой строкой

--- src/com/project/abstract_tree/Node.java
+++ src/com/project/abstract_tree/Node.java
@@ -7,31 +7,39 @@ public class Node <T> {
     private T value;
     private LinkedList<Node> childrens;
     private Node parent;
+
+    // Конструкторы
     public Node(int idNode,T value,Node parent){
         this.idNode=idNode;
         this.value=value;
         this.parent=parent;
     }
+
     public void Node(int idNode,T value){
-        this.idNode=idNode;
-        this.value=value;
-        this.parent=null;
+        this(idNode, value, null);
     }
+
+    //Геттреры и сеттеры атрибутов
     public void setChildren(int idNode,T value,Node parent){
         childrens.add(new Node(idNode,value,parent));
     }
+
     public void setChildren(Node node){
         childrens.add(node);
     }
+
     public LinkedList<Node> getChildrens(){
         return childrens;
     }
+
     public int getIdNode(){
         return idNode;
     }
+
     public void setIdNode(int idNode){
         this.idNode=idNode;
     }
+
     public T getValue() {
         return value;
     }
