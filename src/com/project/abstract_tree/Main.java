9afbb57e2d6d6f23da105d22b4e758e72b9f0c47
SeregeySulimenko <sergey-sulimenko@mail.ru>
2020-12-27 00:05:16 +0400

    добавил javadoc комменты в класс Tree

--- src/com/project/abstract_tree/Main.java
+++ src/com/project/abstract_tree/Main.java
@@ -3,6 +3,25 @@ package com.project.abstract_tree;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Tree myTree = new Tree(new Node<String>(0, "Корень"));
+        System.out.println(myTree.getRoot().getValue());
+        myTree.add(0, new Node<String>(1, "Ветвь 1"));
+        myTree.add(0, new Node<String>(10, "Ветвь 2"));
+        myTree.add(1, new Node<String>(6, "Ветвь 3"));
+        myTree.add(1, new Node<String>(4, "Ветвь 4"));
+        myTree.add(10, new Node<String>(5, "Ветвь 5"));
+        System.out.println("Исходное дерево");
+        System.out.println(myTree.getRoot().getValue());
+        myTree.outputTree(myTree.getRoot());
+        Tree myTree2 = myTree.clone();
+        System.out.println("Клонированное дерево");
+        myTree2.outputTree(myTree.getRoot());
+        System.out.println("Упорядоченное дерево");
+        myTree2.outputTree(myTree.getRoot());
+        /*for (int i = 0; i < 5; i++) {
+            myTree.add(i, new Node<String>(1, "Ветвь"+i));
+            myTree.add(i, new Node<String>(1, "Ветвь"+i));
+        }*/
     }
 }
