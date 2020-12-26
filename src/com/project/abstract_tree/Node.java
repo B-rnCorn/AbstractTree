60300e147bdd71f350335b31c5876d12873f3396
SeregeySulimenko <sergey-sulimenko@mail.ru>
2020-12-27 00:05:16 +0400

    добавил javadoc комменты в класс Tree

--- src/com/project/abstract_tree/Node.java
+++ src/com/project/abstract_tree/Node.java
@@ -1,51 +1,137 @@
 package com.project.abstract_tree;
 
-import java.util.LinkedList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Objects;
 
 public class Node <T>{
-    private int idNode;
+    private int id;
     private T value;
-    private LinkedList<Node> children;
+    private Collection<Node> children;
     private Node parent;
-
-    // Конструкторы
-    public Node(int idNode,T value,Node parent){
-        this.idNode=idNode;
+    private int numChildren=0;
+    /**
+     *Конструкторы
+     */
+    public Node(int id,T value,Node parent){
+        this.id =id;
         this.value=value;
         this.parent=parent;
-        this.children = new LinkedList<>();
+        this.children = new Collection<Node>() {
+            @Override
+            public int size() {
+                return numChildren;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return size()==0;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return Objects.equals(o, this);
+            }
+
+            @Override
+            public Iterator<Node> iterator() {
+                return new Iterator<Node>() {
+                    @Override
+                    public boolean hasNext() {
+                        return next()!=null;
+                    }
+
+                    @Override
+                    public Node next() {
+                        return null;//Я не знаю как обратиться к следующему элементу в общем для колекции
+                    }
+                };
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Node node) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Node> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+        };
     }
 
-    public Node(int idNode,T value){
-        this(idNode, value, null);
+    public Node(int id, T value){
+        this(id, value, null);
     }
 
     public Node(){
         this(0, (T) new Object());
     }
-
-    //Геттреры и сеттеры атрибутов
+     /**
+     *Геттреры и сеттеры атрибутов
+     */
     public void setChildren(int idNode,T value,Node parent){
         children.add(new Node(idNode,value,parent));
     }
 
     public void setChildren(Node node){
+        node.setParent(this);
         children.add(node);
+        numChildren++;
     }
-    public void setChildren(LinkedList<Node> children){
+    public void setChildren(Collection<Node> children){
+        numChildren+=children.size();
         this.children=children;
+        for (Node node:children) {
+            node.setParent(this);
+        }
     }
 
-    public LinkedList<Node> getChildren(){
+    public Collection<Node> getChildren(){
         return children;
     }
 
-    public int getIdNode(){
-        return idNode;
+    public int getId(){
+        return id;
     }
 
-    public void setIdNode(int idNode){
-        this.idNode=idNode;
+    public void setId(int idNode){
+        this.id =idNode;
     }
 
     public T getValue() {
@@ -63,10 +149,11 @@ public class Node <T>{
     public void setParent(Node parent) {
         this.parent = parent;
     }
-
-    //Клонирование
-    public Node cloneNode(){
-        Node cloned= new Node (this.idNode,this.value,this.parent);
+    /**
+     *Клонирование
+     */
+    public Node clone(){
+        Node cloned= new Node (this.id,this.value,this.parent);
         cloned.setChildren(this.children);
         return cloned;
     }
--- src/com/project/abstract_tree/Tree.java
+++ src/com/project/abstract_tree/Tree.java
@@ -1,93 +1,141 @@
 package com.project.abstract_tree;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
 import java.io.*;
 import java.util.Iterator;
-import java.util.LinkedList;
 
+/**
+ * Класс абстрактного дерева
+ * @author Сергей
+ * @author Андрей
+ * @version 1.0
+ */
 public class Tree implements Serializable {
-
-    private Node root;  // Корень дерева
-
-    //Конструкторы
+    /**
+     * Поле для корня дерева
+     */
+    private Node root;
+     /**
+      *Конструктор
+      * @param root - Узел, который будет корнем дерева
+     */
     public Tree(Node root){
         this.root = root;
     }
-
+    /**
+     *Конструктор по умолчанию
+     * @see Tree#Tree(Node)
+     */
     public Tree(){
         this(new Node());
     }
-    //get/set для корня
+     /**
+     *метод get для корня
+     */
     public Node getRoot(){
         return root;
     }
+    /**
+     *метод set для корня
+     * @param root - узел для корня дерева
+     */
     public void setRoot(Node root){
         this.root=root;
     }
-    // Методы
-    //Удаление дерева
+     /**
+     *метод удаление дерева
+     */
     public void deleteTree(){
         this.root = null;
     }
 
-    //Добавление Узла к заданному Родителю
-    public void add(int parentID, Node addingNode){
+    /**
+     * Добавление узла к заданному Родителю
+     * @param parentID - id родителя
+     * @param addingNode - узел дерева для добавления
+     * @return - возвращает результат добавления true-добавлено, false - недобавлено
+     */
+    public boolean add(int parentID, Node addingNode){
         Node parent = search(root, parentID);
-        parent.setChildren(addingNode);
+        if(parent.getId()<addingNode.getId()) {
+            parent.setChildren(addingNode);
+            return true;
+        }else return false;
     }
-
-    //Удаление Узла
+    /**
+     * Удаление Узла
+     * @param nodeID - id узла для удаления
+     */
     public void delete(int nodeID){
         Node parent = search(root, nodeID).getParent();
-        deleteSubNode(nodeID, parent);
+        deleteSub(nodeID, parent);
     }
 
-    public void deleteSubNode(int subNodeID, Node node){
+    /**
+     * Удаление дочернего узла
+     * @param subNodeID
+     * @param node
+     */
+    public void deleteSub(int subNodeID, Node node){
         //Поиск Узла среди дочерних узлов Родителя
         Iterator iterator = node.getChildren().iterator();
 
         while(iterator.hasNext()){
             Node tmp = (Node) iterator.next();
-            if(tmp.getIdNode() == subNodeID) {
+            if(tmp.getId() == subNodeID) {
                 iterator.remove();
                 break;
             }
         }
     }
 
-    // Расщепление ветви дерева
-    public void splitNode(Node splitingNode){
-        int parentID = splitingNode.getParent().getIdNode();
+    /**
+     * Расщепление ветви дерева
+     * @param splittingNode - узел для расщепления
+     */
+    public void split(Node splittingNode){
+        int parentID = splittingNode.getParent().getId();
 
         //Добавить дочерние узлы Разделяемого Узла в список дочерних узлов Родительского Узла
-        LinkedList<Node> list = splitingNode.getChildren();
+        Collection<Node> list = splittingNode.getChildren();
         for (Node child : list){
             add(parentID, child);
         }
 
         //Удалить текущий узел из списка дочерних узлов Родителя
-        deleteSubNode(splitingNode.getIdNode(), splitingNode.getParent());
+        deleteSub(splittingNode.getId(), splittingNode.getParent());
     }
-
-    //Поиск Узла по ключу
+    /**
+     * Поиск узла по ключу
+     * @param nodeID - id узла
+     * @param tmp - вершина для рекурсивного поиска
+     */
     public Node search(Node tmp, int nodeID){
         if (tmp == null) {
             return null;
         }
 
-        if (tmp.getIdNode() == nodeID) {
+        if (tmp.getId() == nodeID) {
             return tmp;
         }
+        Node temp;
+            //Рекурсивно искать Узел среди дочерних узлов
+            Collection<Node> list = tmp.getChildren();
+            for (Node child : list) {
+                temp=search(child,nodeID);
+                if (temp!=null)return temp;
+            }
 
-        //Рекурсивно искать Узел среди дочерних узлов
-        LinkedList<Node> list = tmp.getChildren();
-        for (Node child : list){
-            return search(child, nodeID);
-        }
         return null;
     }
 
+    /**
+     * Метод для сохранения дерева
+     * @param fileName - имя файла для сохранения
+     * @return - файл с деревом
+     */
     //Сохранение Дерева в файл на диск
     public File serializeToFile(String fileName){
         File treeFile = new File(fileName);
@@ -100,6 +148,11 @@ public class Tree implements Serializable {
         }
     }
 
+    /**
+     * Метод для загрузки дерева с файла
+     * @param fileName - имя файла для загрузки
+     * @return - дерево, считаннок из файла
+     */
     //Загрузка Дерева из файла с диска
     public Tree deserializeFromFile(String fileName){
         Tree deserialTree = null;
@@ -112,40 +165,62 @@ public class Tree implements Serializable {
         }
     }
 
+    /**
+     * Метод добавления узла/ветви дерева по id
+     * @param nodeId - id узла, в который добавляется ущел/ветвь
+     * @param nodeAdd - узел/ветвь для добавления
+     */
     //Добавление узла/ветви дерева
     public void addBranch(int nodeId,Node nodeAdd){
         Node destinationNode=search(new Node(),nodeId);
         destinationNode.setChildren(nodeAdd);
         nodeAdd.setParent(destinationNode);
     }
+
+    /**
+     * Метод удаления узла по id
+     * @param nodeId - di узла, который нужно удалить
+     */
     //Удаление узла/ветви дерева
     public void deleteBranch(int nodeId){
         Node delNode=search(new Node(),nodeId);
-        LinkedList<Node> parentChildren =delNode.getParent().getChildren();
+        Collection<Node> parentChildren =delNode.getParent().getChildren();
         Node tempNode;
+        /*
         for(int i=0;i<parentChildren.size();i++) {
             tempNode = parentChildren.get(i);
-            if (tempNode.getIdNode() == nodeId) parentChildren.remove(i);
+            if (tempNode.getId() == nodeId) parentChildren.remove(i);
+        }*/
+        int i=0;
+        for(Node temp:parentChildren){
+            if (temp.getId() == nodeId) parentChildren.remove(i);
+            i++;
         }
     }
+
+    /**
+     * Метод для клонирования дерева
+     * @param tmp - промежуточная переменная для рекурсивного клонирования дерева
+     * @return  - возвращает клонированное дерево
+     */
     //Клонирование дерева
-    private Node cloneTree(Node tmp){
+    private Node clone(Node tmp){
         if (tmp == null) {
             return null;
         }else
         {
-            LinkedList<Node> list = tmp.getChildren();
+            Collection<Node> list = tmp.getChildren();
             for (Node child : list){
-                return cloneTree(child.cloneNode());
+                return clone(child.clone());
             }
         }
         return null;
     }
-    public Tree cloneTree(){
-        return new Tree(cloneTree(this.getRoot()));
+    public Tree clone(){
+        return new Tree(clone(this.getRoot()));
     }
     //Упорядочивание дерева
-    public Tree orderTree(){
+    /*public Tree orderTree(){
         return new Tree(orderTree(this.root));
     }
     private Node orderTree(Node tmp){
@@ -155,10 +230,24 @@ public class Tree implements Serializable {
         {
             LinkedList<Node> list = tmp.getChildren();
             for (Node child : list){
-                if(child.getIdNode()>tmp.getIdNode())deleteBranch(child.getIdNode());
+                if(child.getIdNode()<tmp.getIdNode())deleteBranch(child.getIdNode());
                 else return orderTree(child.cloneNode());
             }
         }
         return null;
+    }*/
+    //вывод дерева на консоль
+    public void outputTree(Node tmp){
+        if (tmp != null&&tmp.getParent()==null){
+            System.out.println("ID: "+tmp.getId()+" Value: "+tmp.getValue());
+        }
+        if (tmp != null&&tmp.getParent()!=null) {
+            System.out.println("ID: "+tmp.getId()+" Value: "+tmp.getValue()+" Parent: "+tmp.getParent().getId());
+        }
+        //Рекурсивно искать Узел среди дочерних узлов
+        Collection<Node> list = tmp.getChildren();
+        for (Node child : list) {
+            outputTree(child);
+        }
     }
 }
