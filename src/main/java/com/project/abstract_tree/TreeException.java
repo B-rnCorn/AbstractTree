611658e7475b18354d9e1ac5fd5988ae56bae6ae
SeregeySulimenko <sergey-sulimenko@mail.ru>
2020-12-28 20:15:35 +0400

    исправил maven

--- src/main/java/com/project/abstract_tree/TreeException.java
+++ src/main/java/com/project/abstract_tree/TreeException.java
@@ -1,4 +1,4 @@
-package com.project.abstract_tree;
+package main.java.com.project.abstract_tree;
 
 /**
  * Исключение, выбрасывающееся при возникновении исключительных ситуаций, свзяанных с деревом
