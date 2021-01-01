da583781ca7baa1d94489d395ff302eb4746f166
SeregeySulimenko <sergey-sulimenko@mail.ru>
2021-01-01 14:11:23 +0400

    добавил JUnit

---
+++ src/test/java/com/project/abstract_tree/TreeTest.java
@@ -0,0 +1,61 @@
+package com.project.abstract_tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+    @Test
+    public void getRoot() {
+        
+    }
+
+    @Test
+    public void setRoot() {
+    }
+
+    @Test
+    public void deleteTree() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void removeNodeById() {
+    }
+
+    @Test
+    public void removeSubNode() {
+    }
+
+    @Test
+    public void splitById() {
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void serialization() {
+    }
+
+    @Test
+    public void deserialization() {
+    }
+
+    @Test
+    public void addBranch() {
+    }
+
+    @Test
+    public void deleteBranch() {
+    }
+
+    @Test
+    public void testClone() {
+    }
+}
