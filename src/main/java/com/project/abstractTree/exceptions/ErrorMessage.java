43d42c6e36757e2e8cf75413b9074e04b91915a4
SeregeySulimenko <sergey-sulimenko@mail.ru>
2021-01-20 01:38:48 +0400

    some minore fixes

---
+++ src/main/java/com/project/abstractTree/exceptions/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.project.abstractTree.exceptions;
+
+public class ErrorMessage {
+    /**
+     * constant for error message in symbol stream input
+     */
+    public static final String I_ERROR_MESSAGE = "Ошибка ввода с символьного потока ";
+    /**
+     * constant for error message in symbol stream output
+     */
+    public static final String O_ERROR_MESSAGE = "Ошибка вывода в символьный поток ";
+    /**
+     * constant for error message in adding branch
+     */
+    public static final String ADDING_BRANCH_ERROR = "Вершина ветви не соответствует уловию упорядоченности дерева";
+
+}
