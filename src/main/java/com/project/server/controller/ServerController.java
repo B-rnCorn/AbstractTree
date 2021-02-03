603cd8947f3570ef18d79a21ec2c124c5caef9e5
Dryuss <gipto93@gmail.com>
2021-02-03 22:30:40 +0400

    fix server-part

--- src/main/java/com/project/server/controller/ServerController.java
+++ src/main/java/com/project/server/controller/ServerController.java
@@ -1,13 +1,13 @@
 package com.project.server.controller;
 
+import com.project.abstractTree.model.Node;
 import com.project.abstractTree.model.Tree;
+import com.project.server.model.Task;
 import com.project.server.service.TreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ServerController {
@@ -19,42 +19,46 @@ public class ServerController {
         this.treeService = treeService;
     }
 
-    @PostMapping(value = "/tasks")
-    public ResponseEntity<?> create(@RequestBody Tree<?> taskTree) {
-        treeService.create(taskTree);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    @PostMapping(value = "/tasks/{id}")
+    public ResponseEntity<Tree<Task>> create(@PathVariable(name = "id") int id, @RequestBody String name) {
+        Tree<Task> taskTree =  treeService.create(id, name);
+
+        return taskTree != null ?
+                new ResponseEntity<>(taskTree, HttpStatus.CREATED)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
     @GetMapping(value = "/tasks")
-    public ResponseEntity<List<Tree<?>>> read() {
-        List<Tree<?>> taskTreesList = treeService.readAll();
+    public ResponseEntity<Tree<Task>> read() {
+        Tree<Task> taskTree = treeService.readAll();
 
-        return taskTreesList != null && !taskTreesList.isEmpty() ?
-                new ResponseEntity<>(taskTreesList, HttpStatus.OK)
-                : new ResponseEntity<>(taskTreesList, HttpStatus.NOT_FOUND);
+        return taskTree != null && taskTree.getRoot() != null ?
+                new ResponseEntity<>(taskTree, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @GetMapping(value = "/tasks/{id}")
-    public ResponseEntity<?> read(@PathVariable(name = "id") int id) {
-        Tree<?> taskTree = treeService.read(id);
+    public ResponseEntity<Node<Task>> read(@PathVariable(name = "id") int id) {
+        Node<Task> taskNode = treeService.read(id);
 
-        return taskTree != null ? new ResponseEntity<>(taskTree, HttpStatus.OK)
-                : new ResponseEntity<>(taskTree, HttpStatus.NOT_FOUND);
+        return taskNode != null ? new ResponseEntity<>(taskNode, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping(value = "/tasks/{id}")
-    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody Tree<?> taskTree) {
-        boolean updated = treeService.update(taskTree, id);
+    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody Task task) {
+        boolean updated = treeService.update(id, task);
 
         return updated ? new ResponseEntity<>(HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
-    @DeleteMapping(value = "/tasks/{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") int id) {
-        boolean deleted = treeService.delete(id);
+    @DeleteMapping(value = "/tasks/{id}?type={keyword}")
+    public ResponseEntity<Node<Task>> delete(@PathVariable(name = "id") int id, @PathVariable (name = "keyword") String type) {
+        Node<Task> parentNode = treeService.delete(id, Boolean.parseBoolean(type));
 
-        return deleted ? new ResponseEntity<>(HttpStatus.OK)
+        return parentNode != null ?
+                new ResponseEntity<>(parentNode, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
--- src/main/java/com/project/server/service/TreeService.java
+++ src/main/java/com/project/server/service/TreeService.java
@@ -1,44 +1,94 @@
 package com.project.server.service;
 
+import com.project.abstractTree.exceptions.TreeException;
+import com.project.abstractTree.model.Node;
 import com.project.abstractTree.model.Tree;
+import com.project.server.model.Task;
+import javafx.scene.Parent;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.project.server.service.Constants.CACHE;
-import static com.project.server.service.Constants.TASKTREE_ID_HOLDER;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class TreeService {
 
-    public void create(Tree<?> taskTree) {
-        final int taskTreeId = TASKTREE_ID_HOLDER.incrementAndGet();
-        taskTree.getRoot().setId(taskTreeId);
-        CACHE.put(taskTreeId, taskTree);
+    // Переменная для хранения дерева
+    private static final Tree<Task> TASK_TREE = new Tree<>();
+
+    // Переменная для генерирования ID задачи
+    public static int TASK_ID_HOLDER;
+
+    public TreeService() {
+        try {
+            TASK_TREE.read(new FileReader("src/main/resources/treeStorageFile.json"));
+            TASK_ID_HOLDER = TASK_TREE.getMaxId();
+        } catch (TreeException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public Tree<?> read(int id) {
-        return CACHE.getIfPresent(id);
+    public Tree<Task> create(int parentId, String taskName) {
+        final int taskId = ++TASK_ID_HOLDER;
+        TASK_TREE.setMaxId(TASK_ID_HOLDER);
+
+        Task newTask = new Task(taskId, taskName);
+        if (TASK_TREE.add(parentId, new Node<>(taskId, newTask))) {
+            try {
+                TASK_TREE.write(new FileWriter("src/main/resources/treeStorageFile.json", false));
+            } catch (TreeException | IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return TASK_TREE;
+        }
+        return null;
     }
 
-    public List<Tree<?>> readAll() {
-
-        return new ArrayList<>(CACHE.asMap().values());
+    public Tree<Task> readAll() {
+        return TASK_TREE;
     }
 
+    public Node<Task> read(int id) {
+        return TASK_TREE.search(id);
+    }
 
-    public boolean update(Tree<?> taskTree, int id) {
-        if (CACHE.asMap().containsKey(id)) {
-            taskTree.getRoot().setId(id);
-            CACHE.put(id, taskTree);
-            return true;
+    public boolean update(int id, Task task) {
+        Node<Task> taskNode = TASK_TREE.search(id);
+
+        if (taskNode != null && id >= 0) {
+            try {
+                taskNode.setValue(task);
+                TASK_TREE.write(new FileWriter("src/main/resources/treeStorageFile.json", false));
+                return true;
+            } catch (TreeException | IOException e) {
+                e.printStackTrace();
+                return false;
+            }
         }
         return false;
     }
 
-    public boolean delete(int id) {
-        CACHE.invalidate(id);
-        return CACHE.getIfPresent(id) == null;
+    public Node<Task> delete(int id, boolean type) {
+        int parentId = TASK_TREE.search(id).getParent().getId();
+
+        if (TASK_TREE.search(id) != null && id > 0) {
+
+            if (type) TASK_TREE.splitById(id);
+            else TASK_TREE.removeNodeById(id);
+
+            try {
+                TASK_TREE.write(new FileWriter("src/main/resources/treeStorageFile.json", false));
+                return TASK_TREE.search(parentId);
+            } catch (TreeException | IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return null;
     }
 }
